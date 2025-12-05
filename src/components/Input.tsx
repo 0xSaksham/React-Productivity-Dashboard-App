a4b2b2e42e4f6fea2d46409c44fefb95d0f1e192
Smrati Sharma <smrati.sharma@admiralindia.com>
2025-12-05 17:50:33 +0530

    Input done

---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import React, { forwardRef } from "react";
+import "./Input.css";
+
+type InputProps = React.ComponentPropsWithoutRef<"input"> & {
+  label?: string;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, id, ...rest }, ref) => {
+    const inputId = id || `input-${Math.random().toString(36).substr(2, 9)}`;
+
+    return (
+      <div className="input-container">
+        {label && <label className="input-label" htmlFor={inputId}>{label}</label>}
+        <input
+          id={inputId}
+          className="input-field"
+          ref={ref}
+          {...rest}
+        />
+      </div>
+    );
+  }
+);
+
+export default Input;
